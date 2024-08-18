92008917a9ad10a3e8e328de1d0bba852d72bca6
Teslule <luletesfaye2020@gmail.com>
2024-08-18 09:40:07 -0400

    front end commit

---
+++ src/Axios/axiosConfig.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  // baseURL: "http://localhost:7700/api",
+
+  baseURL: "https://evangadi-forum-backend-logic-nxrq.vercel.app/api",
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
